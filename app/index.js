d3ad9c7e0bc77e55b2adb3ea3211d7df950875f3
Sukhdeep <sukhdeep1744@gmail.com>
2026-03-16 17:55:45 +0530

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/",(req,res)=>{
+    res.send('Running version v2');
+})
+
+app.listen(PORT, ()=>{
+    console.log(` App is listening on port ${PORT}`);
+})
